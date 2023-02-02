a16d512c668400e8e0fea63dddc546a4c2211f20
Alan Davey <Alan.Davey@dealerbuilt.com>
2023-02-02 13:25:51 -0500

    fixed issue with output not writing to console until Omnis was closed

--- bin/omniscli.ps1
+++ bin/omniscli.ps1
@@ -75,6 +75,7 @@ function flushStream ([System.IO.StreamReader]${file_stream}, [System.IO.StreamW
     while (-not ${file_stream}.EndOfStream) {
         ${output}.Write([char]${file_stream}.Read())
     }
+    ${output}.Flush()
 }
 
 function flushConsole {
